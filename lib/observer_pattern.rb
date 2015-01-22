4cecdbdac3529b726ddb1b7d1923d2a40f449b7c
Andre Kibbe <akibbe02@gmail.com>
2015-01-22 09:32:12 -0800

    Observer pattern first take

--- lib/observer_pattern.rb
+++ lib/observer_pattern.rb
@@ -1,19 +1,35 @@
 class Tile
+  attr_reader :cursed_creatures
 
   def initialize(attrs = {})
     @cursed = attrs.fetch(:cursed, false)
+    @cursed_creatures = []
+  end
+
+  def add_cursed(creature)
+    @cursed_creatures << creature
   end
 
   def cursed?
     @cursed
   end
 
+  def activate_curse
+    cursed_creatures.each { |creature| creature.damage(4) }
+  end
+
 end
 
 class Hero
+  attr_reader :health
 
   def initialize
     @cursed = false
+    @health = 10
+  end
+
+  def damage(hit)
+    @health -= hit
   end
 
   def cursed?
@@ -21,7 +37,10 @@ class Hero
   end
 
   def discover(tile)
-    @cursed = true if tile.cursed?
+    if tile.cursed?
+      @cursed = true
+      tile.add_cursed(self)
+    end
   end
 
 end
--- spec/observer_pattern_spec.rb
+++ spec/observer_pattern_spec.rb
@@ -4,8 +4,9 @@ require_relative '../lib/observer_pattern'
 describe "Name Pattern" do
 
   describe Hero do
+    let(:hero) { Hero.new }
+
     it "is cursed when he discovers cursed Tile" do
-      hero = Hero.new
       tile = Tile.new cursed: true
       hero.discover(tile)
 
@@ -13,19 +14,49 @@ describe "Name Pattern" do
     end
 
     it "is not cursed when he discovers simple Tile without curse" do
-      hero = Hero.new
       tile = Tile.new
       hero.discover(tile)
 
       expect(hero.cursed?).to be_falsey
     end
+
+    it "has default health equal to 10" do
+      expect(hero.health).to eq(10)
+    end
+    it "can be damaged" do
+      hero.damage(6)
+      expect(hero.health).to eq(4)
+    end
   end
 
   describe Tile do
+
+    it "activates curse" do
+      hero = Hero.new
+      tile = Tile.new cursed: true
+      hero.discover(tile)
+
+      tile.activate_curse
+      expect(hero.health).to eq(6)
+    end
+
+    it "activates curse on several heroes" do
+      hero1 = Hero.new
+      hero2 = Hero.new
+      tile = Tile.new cursed: true
+      hero1.discover(tile)
+      hero2.discover(tile)
+
+      tile.activate_curse
+      expect(hero1.health).to eq(6)
+      expect(hero2.health).to eq(6)
+    end
+
     it "is not cursed by default" do
       tile = Tile.new
       expect(tile.cursed?).to be_falsey
     end
+
     it "can be created as cursed" do
       tile = Tile.new cursed: true
       expect(tile.cursed?).to be_truthy
