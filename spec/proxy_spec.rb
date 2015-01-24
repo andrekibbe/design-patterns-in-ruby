b6a9001fc66eabf823b65f9627e3c158c1279a02
Andre Kibbe <akibbe02@gmail.com>
2015-01-23 19:27:18 -0800

    Authorization with proxy

--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -4,12 +4,14 @@ require_relative '../lib/proxy'
 describe "Proxy Pattern" do
 
   it "delegates all functionality to the real object" do
+    hero = Hero.new
     computer = double("computer", queue: [], add: [], execute: true)
-    proxy = ComputerProxy.new(computer)
+    proxy = ComputerProxy.new(computer, hero)
 
     expect(computer).to receive(:add)
     proxy.add(double("command"))
 
+    hero.keywords << :computer
     expect(computer).to receive(:execute)
     proxy.execute
   end
